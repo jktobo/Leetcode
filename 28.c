13cd66af7c5d0cc5fe6933a700dddd48a28238d4
Joldosh <joldosh.k@gmail.com>
2023-01-12 23:41:05 +0400

    28. Find the Index of the First Occurrence in a String

---
+++ 28.c
@@ -0,0 +1,15 @@
+int strStr(char * haystack, char * needle){
+ int n = strlen(haystack), m = strlen(needle);
+    for (int i = 0; i <= n - m; i++) {
+        int j;
+        for (j = 0; j < m; j++) {
+            if (haystack[i + j] != needle[j]) {
+                break;
+            }
+        }
+        if (j == m) {
+            return i;
+        }
+    }
+    return -1;
+}
