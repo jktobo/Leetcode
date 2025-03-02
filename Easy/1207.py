c3c869182b5c8b29810872a28a465ce029517bba
Admin <joldosh.k@gmail.com>
2025-03-02 21:44:33 +0600

    1207. Unique Number of Occurrences

--- Easy/1207.py
+++ Easy/1207.py
@@ -1,3 +1,5 @@
 class Solution:
     def uniqueOccurrences(self, arr: List[int]) -> bool:
-        
+        freq = Counter(arr)
+        occurrences = set(freq.values())
+        return len(occurrences) == len(freq)
